a17c89c8355f5fd3a9ba812c1b1b789e4b18c179
leichen <313399524@qq.com>
2017-02-14 08:34:33 +0800

    index

--- index.php
+++ index.php
@@ -82,5 +82,5 @@ $mageRunCode = isset($_SERVER['MAGE_RUN_CODE']) ? $_SERVER['MAGE_RUN_CODE'] : ''
 
 /* Run store or run website */
 $mageRunType = isset($_SERVER['MAGE_RUN_TYPE']) ? $_SERVER['MAGE_RUN_TYPE'] : 'store';
-Mage::app()->cleanCache();
+
 Mage::run($mageRunCode, $mageRunType);
