067bcac6795d99e654802cc8ed32a6db1f5374d9
leichen <313399524@qq.com>
2017-02-22 23:17:25 +0800

    debug

--- app/code/core/Mage/Rule/Model/Condition/Product/Abstract.php
+++ app/code/core/Mage/Rule/Model/Condition/Product/Abstract.php
@@ -524,6 +524,7 @@ abstract class Mage_Rule_Model_Condition_Product_Abstract extends Mage_Rule_Mode
         }
 
         if ('category_ids' == $attrCode) {
+            Mage::log($object->getAvailableInCategories());
             return $this->validateAttribute($object->getAvailableInCategories());
         } elseif (! isset($this->_entityAttributeValues[$object->getId()])) {
             if (!$object->getResource()) {
